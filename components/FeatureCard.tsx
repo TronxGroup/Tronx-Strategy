49b7e87d888f5047e20b559114f9b4579d9476a3
Tronx Group <tronxgroupspa@gmail.com>
2025-11-17 22:04:46 -0300

    Delete components directory

--- components/FeatureCard.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function FeatureCard({ title, children }: { title: string; children: React.ReactNode }) {
-  return (
-    <div className="card p-6">
-      <h4 className="font-semibold">{title}</h4>
-      <p className="mt-2 text-sm text-slate-600">{children}</p>
-    </div>
-  );
-}
